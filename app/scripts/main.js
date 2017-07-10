e3778b4ffb300da9b0e5a0998b23116a89fec905
Wahhaj Ali <wahhajj@gmail.com>
2017-07-10 02:20:20 -0400

    Added smooth scroll js for navbar links

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -88,4 +88,40 @@
     }
   })
 
+  // Smooth scroll each navbar link to its section
+  const pageLinks = document.querySelectorAll("a[href*='#']")
+  pageLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      scrollToElement(document.querySelector(e.target.getAttribute('href')))
+    })
+  })
+
+  function scrollToElement(element) {
+    const scrollDuration = 250
+    const startingY = window.pageYOffset
+    const elementY = element.offsetTop - navbarHeight
+    const diff = elementY - startingY
+    let start = null
+
+    // Bootstrap our animation - it will get called right before next frame shall be rendered.
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) {
+        start = timestamp
+      }
+
+      // Elapsed miliseconds since start of scrolling.
+      const time = timestamp - start
+
+      // Get percent of completion in range [0, 1].
+      const percent = Math.min(time / scrollDuration, 1)
+
+      window.scrollTo(0, startingY + diff * percent)
+
+      // Proceed with animation as long as we wanted it to.
+      if (time < scrollDuration) {
+        window.requestAnimationFrame(step)
+      }
+    })
+  }
+
 })();
