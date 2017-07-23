8d23e0b3b604cab3bb85ea2f84a2e37777ef6fe7
Wahhaj Ali <wahhajj@gmail.com>
2017-07-23 03:35:21 -0400

    Moved navbar js to its own file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,127 +1,73 @@
 /*!
- *
- *  Web Starter Kit
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+*
+*  Web Starter Kit
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
 /* eslint-env browser */
 (function() {
-  'use strict';
+  'use strict'
 
   // Check to make sure service workers are supported in the current browser,
   // and that the current page is accessed from a secure origin. Using a
   // service worker from an insecure origin will trigger JS console errors. See
   // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+  const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
       // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
       // 127.0.0.1/8 is considered localhost for IPv4.
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-      )
-    );
+      window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
+  )
 
-  if ('serviceWorker' in navigator &&
-      (window.location.protocol === 'https:' || isLocalhost)) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(function(registration) {
-      // updatefound is fired if service-worker.js changes.
-      registration.onupdatefound = function() {
-        // updatefound is also fired the very first time the SW is installed,
-        // and there's no need to prompt for a reload at that point.
-        // So check here to see if the page is already controlled,
-        // i.e. whether there's an existing service worker.
-        if (navigator.serviceWorker.controller) {
-          // The updatefound event implies that registration.installing is set:
-          // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-          var installingWorker = registration.installing;
+  if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then(function(registration) {
+        // updatefound is fired if service-worker.js changes.
+        registration.onupdatefound = function() {
+          // updatefound is also fired the very first time the SW is installed,
+          // and there's no need to prompt for a reload at that point.
+          // So check here to see if the page is already controlled,
+          // i.e. whether there's an existing service worker.
+          if (navigator.serviceWorker.controller) {
+            // The updatefound event implies that registration.installing is set:
+            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+            const installingWorker = registration.installing
 
-          installingWorker.onstatechange = function() {
-            switch (installingWorker.state) {
+            installingWorker.onstatechange = function() {
+              switch (installingWorker.state) {
               case 'installed':
                 // At this point, the old content will have been purged and the
                 // fresh content will have been added to the cache.
                 // It's the perfect time to display a "New content is
                 // available; please refresh." message in the page's interface.
-                break;
+                break
 
               case 'redundant':
-                throw new Error('The installing ' +
-                                'service worker became redundant.');
+                throw new Error('The installing ' + 'service worker became redundant.')
 
               default:
                 // Ignore
+              }
             }
-          };
+          }
         }
-      };
-    }).catch(function(e) {
-      console.error('Error during service worker registration:', e);
-    });
+      })
+      .catch(function(e) {
+        console.error('Error during service worker registration:', e)
+      })
   }
-
-  const navbarHeight = 80
-
-  // Your custom JavaScript goes here
-  window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('nav.navbar')
-    const header = document.querySelector('header.header')
-
-    if (window.pageYOffset >= window.innerHeight - navbarHeight) {
-      navbar.classList.add('fixed')
-      header.classList.add('fixed')
-    } else {
-      navbar.classList.remove('fixed')
-      header.classList.remove('fixed')
-    }
-  })
-
-  // Smooth scroll each navbar link to its section
-  const pageLinks = document.querySelectorAll("a[href*='#']")
-  pageLinks.forEach((link) => {
-    link.addEventListener('click', (e) => {
-      scrollToElement(document.querySelector(e.target.getAttribute('href')))
-    })
-  })
-
-  function scrollToElement(element) {
-    const scrollDuration = 250
-    const startingY = window.pageYOffset
-    const elementY = element.offsetTop - navbarHeight
-    const diff = elementY - startingY
-    let start = null
-
-    // Bootstrap our animation - it will get called right before next frame shall be rendered.
-    window.requestAnimationFrame(function step(timestamp) {
-      if (!start) {
-        start = timestamp
-      }
-
-      // Elapsed miliseconds since start of scrolling.
-      const time = timestamp - start
-
-      // Get percent of completion in range [0, 1].
-      const percent = Math.min(time / scrollDuration, 1)
-
-      window.scrollTo(0, startingY + diff * percent)
-
-      // Proceed with animation as long as we wanted it to.
-      if (time < scrollDuration) {
-        window.requestAnimationFrame(step)
-      }
-    })
-  }
-
-})();
+})()
---
+++ app/scripts/navbar.js
@@ -0,0 +1,53 @@
+;(function() { // eslint-disable-line
+  const navbarHeight = 80
+
+  window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('nav.navbar')
+    const header = document.querySelector('header.header')
+
+    if (window.pageYOffset >= window.innerHeight - navbarHeight) {
+      navbar.classList.add('fixed')
+      header.classList.add('fixed')
+    } else {
+      navbar.classList.remove('fixed')
+      header.classList.remove('fixed')
+    }
+  })
+
+  // Smooth scroll each navbar link to its section
+  const pageLinks = document.querySelectorAll('a[href*=\'#\']')
+
+  pageLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      scrollToElement(document.querySelector(e.target.getAttribute('href')))
+    })
+  })
+
+  function scrollToElement(element) {
+    const scrollDuration = 250
+    const startingY = window.pageYOffset
+    const elementY = element.offsetTop - navbarHeight
+    const diff = elementY - startingY
+    let start = null
+
+    // Bootstrap our animation - it will get called right before next frame shall be rendered.
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) {
+        start = timestamp
+      }
+
+      // Elapsed miliseconds since start of scrolling.
+      const time = timestamp - start
+
+      // Get percent of completion in range [0, 1].
+      const percent = Math.min(time / scrollDuration, 1)
+
+      window.scrollTo(0, startingY + diff * percent)
+
+      // Proceed with animation as long as we wanted it to.
+      if (time < scrollDuration) {
+        window.requestAnimationFrame(step)
+      }
+    })
+  }
+})()
