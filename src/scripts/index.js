096f80af4e4edebf6225caf2732bdfa6a70b6b1f
Wahhaj Ali <github@wahhajali.com>
2019-05-01 03:45:00 -0400

    Replaced Sass with PostCSS (#3)
    * add required loaders
    
    * add postcss config
    
    * update webpack config to process css
    
    * added app.css file
    
    * remove sass files
    
    * update template
    
    * replace linearicons font with svg icons
    
    * import styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
+import css from '../styles/app.css'
 import { drawScene } from "./scene"
 import { startGame } from "./game"
-
 drawScene()
 startGame()
--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,22 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader'
+        ]
+      }
     ],
   },
   plugins: [
     new CopyPlugin([
       { from: 'src/manifest.json' },
-      { from: 'src/favicon.ico' },
       { from: 'src/robots.txt' },
       { from: 'src/index.html' },
+      { from: 'src/images/', to: 'images/' }
     ]),
   ],
 
